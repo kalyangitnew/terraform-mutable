98087b6ca8c096e8e6ec6460acc7b0a6abee31f8
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-26 22:06:40 +0530

    TERRAFORM-MUTABLE

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -2,6 +2,6 @@ resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_ID
   tags = {
-    Name = "$(var.ENV)-vpc-to-default-vpc"
+    Name = "${var.ENV}-vpc-to-default-vpc"
   }
 }
