b7c75f0373a9e1beddb7357e95a89704a9a1a005
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-27 00:02:08 +0530

    TERRAFORM-MUTABLE

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -21,25 +21,10 @@ resource "aws_route_table" "route" {
     Name = "example"
   }
 }
-resource "aws_route" "r" {
-  route_table_id = aws_route_table.route.id
-  destination_cidr_block = var.DEFAULT_VPC_CIDR
+
+resource "aws_route" "route-from-default-vpc" {
+  count                     = length(data.aws_route_tables.default-vpc-routes.ids)
+  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
+  destination_cidr_block    = tomap(element(local.association-list, count.index))["cidr"]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
-#{
-# cidr_block = var.DEFAULT_VPC_CIDR
-# vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-# carrier_gateway_id           = ""
-# destination_prefix_list_id = ""
-# egress_only_gateway_id     = ""
-# gateway_id                 = ""
-# instance_id                = ""
-# ipv6_cidr_block            = ""
-# local_gateway_id           = ""
-# nat_gateway_id             = ""
-# network_interface_id      = ""
-# transit_gateway_id         = ""
-# vpc_endpoint_id           = ""
-
-#}
-#
