d17d3bd668b6f64e30d733cb5dc37e82c5396b6c
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-29 21:32:30 +0530

    TERRAFORM-MUTABLE

---
+++ db/env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-practice"
+key    = "terraform-mutable/db/prod/terraform.tfstate"
+region = "us-east-1"
---
+++ db/mysql-rds.tf
@@ -0,0 +1,23 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "dummy"
+  username             = "admin"
+  password             = "admin123"
+  parameter_group_name = aws
+  skip_final_snapshot  = true
+}
+resource "aws_db_parameter_group" "pg" {
+  name   = "mysql-${var.ENV}-pg"
+  family = "mysql5.7"
+}
+resource "aws_db_subnet_group" "default" {
+  name       = "mysqldb-subnet-group-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS
+
+  tags = {
+    Name = "mysqldb-subnet-group-${var.ENV}"
+  }
+}
