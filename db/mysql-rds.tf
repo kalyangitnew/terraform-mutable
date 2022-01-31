ec4b541014323b9aa5be6475568cc96bccf80e65
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-31 12:49:41 +0530

    TERRAFORM-MUTABLE

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,7 +1,8 @@
 locals {
-  rds_user = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_USER"]
-  rds_pass = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_PASS"]
+  rds_user = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  rds_pass = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_PASS"]
 }
+
 
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
