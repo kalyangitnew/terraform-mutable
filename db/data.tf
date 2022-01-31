3c4ca77867d88f399d95ace42a6379ad92265dc5
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-31 13:28:43 +0530

    TERRAFORM-MUTABLE

--- db/data.tf
+++ db/data.tf
@@ -15,3 +15,8 @@ data "aws_secretsmanager_secret" "secrets" {
 data "aws_secretsmanager_secret_version" "secrets-version" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "base"
+  owners      = ["self"]
+}
