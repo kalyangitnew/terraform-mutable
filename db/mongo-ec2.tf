ca0ed520b3ea110608b9374cd5b23e0311174f40
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-31 12:51:05 +0530

    TERRAFORM-MUTABLE

--- db/mongo-ec2.tf
+++ db/mongo-ec2.tf
@@ -1,10 +1,1 @@
 
-resource "aws_rds_cluster" "docdb" {
-  cluster_identifier      = "mongodv-${var.ENV}"
-  engine                  = "docdb"
-  engine_version          = "4.0.0"
-  master_username         = "foo"
-  master_password         = "bar"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-}
