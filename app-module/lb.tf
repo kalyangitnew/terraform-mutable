354e0aeb7a82bd6fee50b2009e68ef02f51db8ef
kalyangitnew <venkatakalyan9704@gmail.com>
2022-02-01 16:31:14 +0530

    TERRAFORM-MUTABLE

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -35,3 +35,29 @@ resource "aws_lb_listener_rule" "private" {
     }
   }
 }
+resource "aws_lb_listener_rule" "public" {
+  count        = var.IS_PRIVATE_LB ? 0 : 1
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = var.LB_RULE_PRIORITY
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
+  }
+}
+resource "aws_lb_listener" "private_listener" {
+  count        = var.IS_PRIVATE_LB ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.example.id
+    type             = "forward"
+  }
+}
