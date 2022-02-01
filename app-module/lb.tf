7627541fc00b1c2b37e3965c58a4a9fa9dee82b0
kalyangitnew <venkatakalyan9704@gmail.com>
2022-02-01 17:00:16 +0530

    TERRAFORM-MUTABLE

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -37,7 +37,7 @@ resource "aws_lb_listener_rule" "private" {
 }
 resource "aws_lb_listener_rule" "public" {
   count        = var.IS_PRIVATE_LB ? 0 : 1
-  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  listener_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
   priority     = var.LB_RULE_PRIORITY
 
   action {
